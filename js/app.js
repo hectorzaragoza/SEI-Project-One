fcdd45bc6f69d862076433f18c9688d0ad4d4c3b
hectorzaragoza <zaragoza09@gmail.com>
2021-10-25 10:21:13 -0500

    got hazard to move across screen automatically

--- js/app.js
+++ js/app.js
@@ -8,6 +8,8 @@
     //Get our canvas element via the id
     const canvas = document.getElementById('canvasArea');
     const h1 = document.getElementById('heading')
+    //
+
 
     //Set the dimensions of the canvas area to be responsive by using getComputedStyle
     // game.setAttribute('width', getComputedStyle(game)['width'])
@@ -48,10 +50,75 @@
     // ctx.fillRect(100,100, 10, 10)
 
     //Create objects: Player, Hazard, Two Establishments
-    const player = new Objects(10,10,10,10, 'red')
-    const hazard = new Objects(20,20,15,15,'red')
-    //Test that render method works
-    player.render()
-    hazard.render()
+    const player = new Objects(25,105,10,10, 'red')
+    const healthUp = new Objects (600,50,40,40, 'green')
+    const healthDown = new Objects (600,150,40,40, 'red')
+    const safety = new Objects (10,90,40,40, 'blue')
+    const hazard = new Objects(100,250,15,15,'red')
+    // //Test that render method works
+    // player.render()
+    // hazard.render()
 
+    //We need to control the movement of our player using the WASD keys in 
+    //addition to keeping the player moving out side the canvas.
+    let moveControl = (e) => {
+        switch(e.key.toLowerCase()) {
+            case ('w'):
+                player.y -= 10
+                if (player.y <= 0) {
+                    player.y = 0
+                }
+                break
+            case ('a'):
+                player.x -= 10
+                if (player.x <= 0) {
+                    player.x = 0
+                }
+                break
+            case ('s'):
+                player.y += 10
+                if(player.y + player.height >= canvas.height) {
+                    player.y = canvas.height - player.height
+                }
+                break
+            case ('d'):
+                player.x += 10
+                if (player.x + player.width >= canvas.width) {
+                    player.x = canvas.width - player.width
+                }    
+                break
+        }
+    } 
+    
+
+    document.addEventListener('keydown', moveControl)
+
+    //Build a function that will loop and update the x,y, coordinates
+    //of my player to render moevement using the moveControl function
+    const gameLoop = () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        moveControl.innerContent = `X: ${player.x}\nY: ${player.y}`
+        safety.render()
+        player.render()
+        healthUp.render()
+        healthDown.render()
+        if(hazard.y > 0) {
+            hazard.y -= 20
+            hazard.render()
+        } else {
+            hazard.y = 250
+        }
+    }
+
+    let gameInterval = setInterval(gameLoop, 70)
+
+    //I need to have an object go from outside the canvas, bottom up, automatically
+    //at a set rate.
+    //I will need: 
+    //  1. an object
+    //  2. a movement control method (maybe a loop because no keys
+    //      are used to manipulate movement)
+
+        
+        
     
